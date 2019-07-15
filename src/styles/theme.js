d3a48ee6a00fd35ebc3474fbcb5cc55313c0ce48
Cliff Barendsen <cliff.barendsen@gmail.com>
2019-07-15 16:17:10 -0700

    removing console.log

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,8 +2,6 @@ import { red } from '@material-ui/core/colors';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { Montserrat, MontserratSemiBold, MontserratThin } from './fonts';
 
-console.log('Montserrat', Montserrat);
-
 // A custom theme for this app
 const theme = createMuiTheme({
   typography: {
